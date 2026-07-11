2dcc944215520377a00586a16c3b037a97402815
Zenvok-arch <218113568+Zenvok-arch@users.noreply.github.com>
2026-07-11 19:45:58 +0530

    getComputerChoice is working

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-console.log("Hello World")
+function getComputerChoice(){
+    const game = ["Rock" , "Paper", "Scissor"]
+    let result;
+    let random = Math.floor( Math.random() * 3 + 1 )
+    if(random == 1 ){
+        result = game[0]
+    }
+    else if(random == 2){
+        result = game[1]
+    }
+    else{
+        result = game[2]
+    }
 
+    return result
+
+}
+
+console.log(getComputerChoice())
