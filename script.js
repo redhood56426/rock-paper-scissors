df0d93a475cd8b524fd034d6b6b9b81a5cba583b
Zenvok-arch <218113568+Zenvok-arch@users.noreply.github.com>
2026-07-11 21:45:19 +0530

    playRound:gets choices

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-function getComputerChoice(){
-    const game = ["Rock" , "Paper", "Scissors"]
+let humanScore = 0;
+let computerScore = 0;
+
+
+
+function getComputerChoice() {
+    const game = ["rock", "paper", "scissors"]
     let result;
-    let random = Math.floor( Math.random() * 3 + 1 )
-    if(random == 1 ){
+    let random = Math.floor(Math.random() * 3 + 1)
+    if (random == 1) {
         result = game[0]
     }
-    else if(random == 2){
+    else if (random == 2) {
         result = game[1]
     }
-    else{
+    else {
         result = game[2]
     }
 
@@ -16,8 +21,45 @@ function getComputerChoice(){
 
 }
 
-function getHumanChoice(){
+function getHumanChoice() {
     let input = prompt("Enter Your Choice : Rock , Paper , Scissors")
-    return console.log(input)
+    return input.toLowerCase().trim()
 }
-getHumanChoice()
+
+function playRound(humanChoice, computerChoice) {
+   
+    if(humanChoice == computerChoice){
+        console.log("It's A draw")
+    }
+    else if(humanChoice === "rock" && computerChoice === "paper"){
+        console.log(`You Lose! ${computerChoice} beats ${humanChoice}`)
+        computerScore++;
+    }
+      else if(humanChoice === "paper" && computerChoice === "scissors"){
+        console.log(`You Lose! ${computerChoice} beats ${humanChoice}`)
+        computerScore++;
+    }
+      else if(humanChoice === "scissors" && computerChoice === "rock"){
+        console.log(`You Lose! ${computerChoice} beats ${humanChoice}`)
+        computerScore++;
+    }
+      else if(humanChoice === "rock" && computerChoice === "scissors"){
+        console.log(`You won! ${humanChoice} beats ${computerChoice}`)
+        humanScore++;
+    }
+       else if(humanChoice === "paper" && computerChoice === "rock"){
+        console.log(`You won! ${humanChoice} beats ${computerChoice}`)
+        humanScore++;
+    }
+       else if(humanChoice === "scissors" && computerChoice === "paper"){
+        console.log(`You won! ${humanChoice} beats ${computerChoice}`)
+        humanScore++;
+    }
+    else{
+        console.log("You Choosed Something Wrong")
+    }
+    
+
+} 
+
+playRound(getHumanChoice(),getComputerChoice())
